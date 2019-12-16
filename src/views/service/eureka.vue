dc35191e6645b8f7b20603f58422eabd10979b56
TunerRed <codevoid001@gmail.com>
2019-12-16 12:32:22 +0800

    eureka页面调整

--- src/views/service/eureka.vue
+++ src/views/service/eureka.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+      <h5>服务列表</h5>
+      <el-divider><i class="el-icon-cpu"></i></el-divider>
       <el-row style="padding-bottom: 15px;">
         <el-select size="small" v-model="serverIP">
           <el-option v-for="(serverIP, index) in serverIPList" :key="index" :label="serverIP" :value="serverIP"></el-option>
@@ -14,7 +16,9 @@
         v-loading="loading"
       >
         <el-table-column label="应用" prop="name" min-width="150" align="center" fixed="left"></el-table-column>
-        <el-table-column label="JAR文件" prop="jar" min-width="220" align="center"></el-table-column>
+        <el-table-column label="JAR文件" prop="jar" min-width="220" align="center">
+          <template slot="header" slot-scope="scoped"><span>文件 </span><el-input v-model="search" size="mini" clearable placeholder="筛选jar" style="margin-top: 7px;padding: 0;width: 80%;"></el-input></template>
+        </el-table-column>
         <el-table-column label="进程" prop="pid" min-width="70" align="center"></el-table-column>
         <el-table-column label="端口" prop="port" min-width="70" align="center"></el-table-column>
         <el-table-column label="内存占用" prop="mem" min-width="85" align="center"></el-table-column>
@@ -27,7 +31,6 @@
           </template>
         </el-table-column>
         <el-table-column label="操作" min-width="140" align="center" fixed="right">
-          <template slot="header" slot-scope="scoped"><el-input v-model="search" size="mini" clearable placeholder="筛选jar" style="margin-top: 7px;padding: 0;"></el-input></template>
           <template slot-scope="scoped">
             <!-- 操作按钮 -->
             <el-button v-if="scoped.row.pid<=0" type="primary" size="small" @click="onOperation('start', scoped.row)" :disabled="btnStartDisabled(scoped.row)">启动</el-button>
@@ -56,29 +59,29 @@
         },
         methods: {
             async initData() {
-                this.loading = true
-                const data = await this.$api.service.getAvailableServerList()
-                this.loading = false
-                this.serverIPList = data.resultData.list
+                this.loading = true;
+                const data = await this.$api.service.getAvailableServerList();
+                this.loading = false;
+                this.serverIPList = data.resultData.list;
                 if (this.serverIPList.length > 0){
                     this.serverIP = this.serverIPList[0]
                 }
             },
             async onSearch() {
                 this.loading = true
-                let result = await this.$api.service.getEurekaList(this.serverIP)
+                let result = await this.$api.service.getEurekaList(this.serverIP);
                 this.loading = false;
-                this.tableData = result.resultData.list
+                this.tableData = result.resultData.list;
                 this.$message({message: '查询完成',type: 'success'});
             },
             async onOperation(val, row) {
                 if (val === 'stop') {
-                    await this.$api.service.stopService(this.serverIP, row.name, row.pid)
-                    this.$message.warning('service done')
-                    row.pid = 0
+                    await this.$api.service.stopService(this.serverIP, row.name, row.pid);
+                    this.$message.warning('service done');
+                    row.pid = 0;
                 } else if (val === 'start') {
-                    row.pid=-1
-                    await this.$api.service.startService(this.serverIP, row.name)
+                    row.pid = -1;
+                    await this.$api.service.startService(this.serverIP, row.name);
                     this.$message.success('服务启动中，请稍后刷新列表或在首页查看')
                 }
             },
